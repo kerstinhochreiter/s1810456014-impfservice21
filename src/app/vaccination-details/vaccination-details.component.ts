98d3f079c983033f9c7e6a9d8d436b96d92f5044
kerstinhochreiter <kerli.hochreiter@gmail.com>
2021-05-17 11:54:56 +0200

    2 files modified

--- src/app/vaccination-details/vaccination-details.component.ts
+++ src/app/vaccination-details/vaccination-details.component.ts
@@ -1,13 +1,13 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-import { Vaccination } from "../shared/vaccination";
-import { Location } from "../shared/location";
-import { VaccinationStoreService } from "../shared/vaccination-store.service";
-import { LocationStoreService } from "../shared/location-store.service";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Vaccination } from '../shared/vaccination';
+import { Location } from '../shared/location';
+import { VaccinationStoreService } from '../shared/vaccination-store.service';
+import { LocationStoreService } from '../shared/location-store.service';
 
 @Component({
-  selector: "is-vaccination-details",
-  templateUrl: "./vaccination-details.component.html"
+  selector: 'is-vaccination-details',
+  templateUrl: './vaccination-details.component.html'
 })
 export class VaccinationDetailsComponent implements OnInit {
   @Input() vaccination: Vaccination;
@@ -27,17 +27,17 @@ export class VaccinationDetailsComponent implements OnInit {
     //man holt sich die gesamte Route und durch snapshot params bekommt man z.B :isbn
     const params = this.route.snapshot.params;
     //gibt mir die genau dieses Buch mit der ISBN
-    this.is_loc.getSingle(params["id"]).subscribe(res => (this.location = res));
-    this.is.getSingle(params["id"]).subscribe(res => (this.vaccination = res));
+    this.is_loc.getSingle(params['id']).subscribe(res => (this.location = res));
+    this.is.getSingle(params['id']).subscribe(res => (this.vaccination = res));
   }
 
   removeVaccination() {
-    if (confirm("Wollen Sie den Impftermin wirklich löschen?")) {
+    if (confirm('Wollen Sie den Impftermin wirklich löschen?')) {
       //asynchron!! --> Inhalt von Subscribe wird erst ausgeführt wenn REST Call fertig ist
       //Was verlang jeder asynchroner aufruf?
       //eine Callback Funktion
       this.is.remove(this.vaccination.id).subscribe(res => {
-        this.router.navigate(["../"], { relativeTo: this.route });
+        this.router.navigate(['../'], { relativeTo: this.route });
       });
     }
   }
