fb81bce83944d34b6ca80777adcecd1f34d7d183
kerstinhochreiter <kerli.hochreiter@gmail.com>
2021-05-17 13:04:08 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { AuthenticationService } from './shared/authentication.service';
 import { Vaccination } from './shared/vaccination';
 
 @Component({
@@ -6,4 +7,23 @@ import { Vaccination } from './shared/vaccination';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+
+
+
+export class AppComponent {
+
+constructor(private authService:AuthenticationService){}  
+
+  isLoggedIn(){
+    return this.authService.isLoggedIn();
+  }
+
+  getLoginStatus(){
+    if(this.isLoggedIn()){
+      return "Logout";
+    } else {
+      return "Login";
+    }
+  }
+
+}
