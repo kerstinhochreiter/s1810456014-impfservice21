819f9080358abaaed5cd70a4d64aaa9606a6e5d2
kerstinhochreiter <kerli.hochreiter@gmail.com>
2021-05-25 10:08:40 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from "@angular/core";
-import { VaccinationListComponent } from "./vaccination-list/vaccination-list.component";
-import { RouterModule, Routes } from "@angular/router";
-import { VaccinationDetailsComponent } from "./vaccination-details/vaccination-details.component";
-import { LoginComponent } from "./login/login.component";
-import { VaccinationFormComponent } from "./vaccination-form/vaccination-form.component";
+import { NgModule } from '@angular/core';
+import { VaccinationListComponent } from './vaccination-list/vaccination-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { VaccinationDetailsComponent } from './vaccination-details/vaccination-details.component';
+import { LoginComponent } from './login/login.component';
+import { VaccinationFormComponent } from './vaccination-form/vaccination-form.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "vaccinations", pathMatch: "full" },
-  { path: "vaccinations", component: VaccinationListComponent },
-  { path: "vaccinations/:id", component: VaccinationDetailsComponent },
-  { path: "login", component: LoginComponent },
-  { path: "admin", component: VaccinationFormComponent },
-    { path: "admin/:id", component: VaccinationFormComponent }
+  { path: '', redirectTo: 'vaccinations', pathMatch: 'full' },
+  { path: 'vaccinations', component: VaccinationListComponent },
+  { path: 'vaccinations/:id', component: VaccinationDetailsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: VaccinationFormComponent },
+  { path: 'admin/:id', component: VaccinationFormComponent }
 ];
 
 @NgModule({
